c15e2a02d9ca4e42fbc719800e93c7c743f2467b
Pelle <dmytri58@gmail.com>
2021-01-03 21:31:26 +0100

    test

--- javaFramework/src/model/ScreenLeaveAction.java
+++ javaFramework/src/model/ScreenLeaveAction.java
@@ -1,5 +1,25 @@
 package model;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class ScreenLeaveAction {
+import eea.engine.action.Action;
+import eea.engine.component.Component;
+import eea.engine.entity.Entity;
+
+public class ScreenLeaveAction implements Action {
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2, Component arg3) {
+		Entity owner = arg3.getOwnerEntity();
+	    if(owner.getPosition().x < 0) {
+	    	owner.setPosition(new Vector2f(1200, owner.getPosition().y));
+	    }
+	    else if(owner.getPosition().x > 1200) {
+	    	owner.setPosition(new Vector2f(0, owner.getPosition().y));
+	    }
+	    System.out.println("Leeeave");
+		
+	}
 
 }
